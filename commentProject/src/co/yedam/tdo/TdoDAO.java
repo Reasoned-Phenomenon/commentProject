2ceed4692952839fbd601e9e005b8993b721d984
RP <rp177@naver.com>
2021-10-09 21:25:11 +0900

    1009 가완성

--- commentProject/src/co/yedam/tdo/TdoDAO.java
+++ commentProject/src/co/yedam/tdo/TdoDAO.java
@@ -1,18 +1,97 @@
 package co.yedam.tdo;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import co.yedam.common.DAO;
 
 public class TdoDAO extends DAO{
 
-	public void showList () {
+	public List<Tdo> showList () {
+		
+		connect();
+		List<Tdo> list = new ArrayList<>();
+		
+		String sql = "SELECT id, content FROM tdl ORDER BY 1";
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+			Tdo tdo = new Tdo();
+			tdo.setId(rs.getString("id"));
+			tdo.setContent(rs.getString("content"));
+			
+			list.add(tdo);
+			}
+			
+			return list;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return list;
+	}
+	
+	public Tdo addList (Tdo tdo) {
+		
+		connect();
+		
+		try {
+			
+			String sql = "INSERT INTO tdl(id,content) VALUES(tdl_seq.nextval, ? ) ";
+			
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, tdo.getContent());
+			int r = psmt.executeUpdate();
+			System.out.println(r+"건 입력");
+			//id값 가져오는 게 문제
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM tdl ORDER BY 1");
+			
+			while ( rs.next()) {
+				tdo.setId(rs.getString("id"));
+			}
+			
+			return tdo;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		
+		return tdo;
 		
 	}
 	
-	public void addList () {
+	public String deleteList (String id) {
 		
-	}
-	
-	public void deleteList () {
+		connect();
+		
+		String sql = "DELETE FROM tdl WHERE id=?";
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			int r = psmt.executeUpdate();
+			System.out.println(r + "건 삭제.");
+			
+			return id;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+			
+		} finally {
+			disconnect();
+		}
+		
+		
 		
 	}
 
--- commentProject/src/co/yedam/tdo/TdoServlet.java
+++ commentProject/src/co/yedam/tdo/TdoServlet.java
@@ -1,11 +1,17 @@
 package co.yedam.tdo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @WebServlet("/tdoServlet")
 public class TdoServlet extends HttpServlet {
@@ -20,7 +26,61 @@ public class TdoServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		Gson gson = new GsonBuilder().create();
+		
+		String cmd = request.getParameter("cmd");
+		TdoDAO dao = new TdoDAO();
+		
+		if ( cmd == null ) {
+			
+			out.println("<h1>빈 페이지입니다.</h1>");
+			
+		} else if (cmd.equals("list")) {
+			
+			List<Tdo> list = dao.showList();
+			out.println(gson.toJson(list));
+			
+		} 
+		
+		else if (cmd.equals("add")) {
+			
+			String content = request.getParameter("content");
+			Tdo tdo = new Tdo();
+			tdo.setContent(content);
+			
+			dao.addList(tdo);
+			
+			out.println(gson.toJson(tdo));
+			
+		}  
+		
+		
+		else if (cmd.equals("del")) {
+			
+			String id = request.getParameter("id");
+			
+			if(dao.deleteList(id) == null) {
+				
+				//{"retCode":"fail"}
+				out.println("{\"retCode\":\"fail\"}");
+				return;
+				
+			} else {
+				
+				out.println("{\"retCode\":\"success\"}");
+				
+			}
+			
+		} 
+	
+	
+	
 	}
 
 }
